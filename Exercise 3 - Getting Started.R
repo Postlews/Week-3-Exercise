6343b1c9194499854767aeac7e8633f9cf949919
Steele Postlewaite <steelepostlewaite@gmail.com>
2022-05-13 03:44:54 -0700

    Finished Task 5

--- Exercise 3 - Getting Started.R
+++ Exercise 3 - Getting Started.R
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
+
 ##Loading any and all packages I may need
-=======
-#Loading any and all packages I may need
->>>>>>> 78597389f6b6b7481015edc18dcd7821d074725d
+
 
 library(readr)
 library(dplyr)    
@@ -10,39 +8,31 @@ library(ggplot2)
 library(sf)
 library(terra)
 library(lubridate)
-<<<<<<< HEAD
 library(SimilarityMeasures)
 library(tidyr)
 library(ggpubr)
-
-##Importing dataset
-=======
-install.packages("SimilarityMeasures")
 library(SimilarityMeasures)
 library(tidyr)
-install.packages("ggpubr")
 library(ggpubr)
 
 #Importing dataset
->>>>>>> 78597389f6b6b7481015edc18dcd7821d074725d
 
 caro60 <- read.csv("C:/Users/Steele/Desktop/PatternTrends/Week 3/Exercise 3/Exercise 3 - First draft/caro60.txt")
 
 caro <- caro60
 
-<<<<<<< HEAD
+
 ## Onto task 1
 
 # TASK 1 - Segmentation Station
 
 ##Since our sampling window is only 1 minute, and we need to use a temporal window of 6 minutes, our window size is will have different positions. 3 minutes before, 2 minutes before, 3 minutes after, etc.
-=======
+
 # Onto task 1
 
 # TASK 1 - Segmentation Station
 
 #Since our sampling window is only 1 minute, and we need to use a temporal window of 6 minutes, our window size is will have different positions. 3 minutes before, 2 minutes before, 3 minutes after, etc.
->>>>>>> 78597389f6b6b7481015edc18dcd7821d074725d
 
 caro <- caro %>%
   mutate(
@@ -54,14 +44,13 @@ caro <- caro %>%
     nPlus3  = sqrt((E-lead(E,3))^2+(N-lead(N,3))^2)
   )
 
-<<<<<<< HEAD
+
 ## Test github connection, push
 
 #Task 2 - Specify and Apply Threshold "d"
 
 ## Attempting to calculate the mean distance for the rows
-=======
->>>>>>> 78597389f6b6b7481015edc18dcd7821d074725d
+
 
 caro <- caro %>%
   rowwise() %>%
@@ -70,7 +59,7 @@ caro <- caro %>%
   ) %>% ungroup()
 
 
-<<<<<<< HEAD
+
 ##Exploring 
 
 ggplot(data=caro, mapping = aes(x=stepMean))+ geom_histogram(binwidth = 0.5)
@@ -141,9 +130,24 @@ caro5  %>%
 
 
 
+# Task 5 - Similarity measures
+
+
+pedestrian <- read.csv("C:/Users/Steele/Desktop/PatternTrends/Week 3/Exercise 3/Exercise 3 - First draft/pedestrian.txt")
+
+
+## viewing trajectories
+
+
+pedestrian %>%
+  ggplot(aes(y=N, x=E)) +
+  geom_path()+ geom_point(aes(col=TrajID)) + 
+  coord_equal() + 
+  facet_wrap(~TrajID,nrow=2) +
+  labs(title="Visual comparison of the 6 trajectories")
 
 
 
-=======
->>>>>>> 78597389f6b6b7481015edc18dcd7821d074725d
+
+
 
